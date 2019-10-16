ff6bcc29069d5c258300ea72d70fc4839fead109
MiguDeveloper <miguel.peru.seo@gmail.com>
2019-10-16 16:25:47 -0500

    inicio de proyecto

---
+++ src/test/java/pe/tuna/proyecto1/Proyecto1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.tuna.proyecto1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
